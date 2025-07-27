8233ca58f48f0da007a8e0c1fda1eaeddf8effc9
ahmed824 <ahmedwarda536@gmail.com>
2025-07-27 16:41:21 +0300

    Refactor about page styles and structure; update section titles and descriptions for improved readability and responsiveness. Add new CSS rules for section title and description styling.

--- about.php
+++ about.php
@@ -6,9 +6,11 @@
 include 'includes/breadcrumb.php'; ?>
 <div class="about-page  ">
   <div class="head text-center w-75 mb-5 m-auto">
-    <h2>what we do</h2>
-    <p>We provide comprehensive advisory services in legal, financial, and business domains, helping you make informed
-      decisions backed by deep expertise and market insight.</p>
+    <h2 class="section-title">What we do</h2>
+    <p class="section-description">
+      We provide comprehensive advisory services in legal, financial, and business domains, helping you make informed
+      decisions backed by deep expertise and market insight.
+    </p>
 
   </div>
   <div class="mb-5">
@@ -58,13 +60,13 @@ include 'includes/breadcrumb.php'; ?>
             </div>
           </div>
           <p class="category-description-second mt-5">
-              <span class="highlight-blue">Mission</span> provide comprehensive advisory services in legal, financial,
-              and business domains, helping you
-              make informed decisions backed by deep expertise and market insight. We provide comprehensive
-              advisory services in legal, financial, and business domains, helping you make informed decisions
-              backed by deep expertise and market insight.We provide comprehensive advisory services in legal,
-              financial, and business domains,
-            </p>
+            <span class="highlight-blue">Mission</span> provide comprehensive advisory services in legal, financial,
+            and business domains, helping you
+            make informed decisions backed by deep expertise and market insight. We provide comprehensive
+            advisory services in legal, financial, and business domains, helping you make informed decisions
+            backed by deep expertise and market insight.We provide comprehensive advisory services in legal,
+            financial, and business domains,
+          </p>
         </div>
       </div>
 
